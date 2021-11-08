119e3c9873dc775c7f318ea15c18290f3bf7d6b2
Michelle Mason <michellemason95@yahoo.com>
2021-11-08 18:31:52 +0000

    fully functional
    git status\\

--- app.js
+++ app.js
@@ -1,47 +1,37 @@
 //API KEY: eLxUOxv6lwapMJ0vJ8ZNtd9z20VyBH31
 //api.giphy.com/v1/gifs/random	
 
-const input = document.querySelector('#giphInput');
+
 const form = document.querySelector('#searchForm');
 const giphyArea = document.querySelector('#gif-area');
 
 function createGiphy(res) {
-    // let newDiv = document.createElement('div');
-    // newDiv.classList.add('col');
+    let newDiv = document.createElement('div');
+    newDiv.classList.add('col');
 
     let img = document.createElement('img');
     img.src = res;
 
-    // newDiv.append(img);
-    giphyArea.append(img);
+    newDiv.append(img);
+    giphyArea.append(newDiv);
     return res;
 }
 
+
 form.addEventListener('submit', async function(e) {
     e.preventDefault();
+    let input = document.querySelector('#giphInput');
 
-    const response = await axios.get('https://api.giphy.com/v1/gifs/random', {params: {api_key: 'eLxUOxv6lwapMJ0vJ8ZNtd9z20VyBH31',
-    q: input}});
+    const response = await axios.get('https://api.giphy.com/v1/gifs/random', {params: {
+        api_key: 'eLxUOxv6lwapMJ0vJ8ZNtd9z20VyBH31',
+        tag: input.value}} );
+    // console.log(response.data.data.images);
     let imgURL = response.data.data.images.downsized_medium.url;
-    // console.log(response.data.data.images.downsized_medium.url);
+    input.value = '';
     createGiphy(imgURL);
 });
 
-
-// async function getGiphy(query) {
-//     const result = await axios.get('https://api.giphy.com/v1/gifs/random', {params: {api_key: 'eLxUOxv6lwapMJ0vJ8ZNtd9z20VyBH31',
-//     q: query}});
-//     const randomImgURL = result.data.data.url;
-//     createGiphy(randomImgURL);
-// }
-
-// function createGiphy(res) {
-//     let newDiv = document.createElement('div');
-//     newDiv.classList.add('col');
-
-//     let img = document.createElement('img');
-//     img.src = res;
-
-//     newDiv.append(img);
-//     giphyArea.append(newDiv);
-// }
+const deleteBtn = document.getElementById('delete');
+deleteBtn.addEventListener('click', function() {
+    giphyArea.innerHTML = '';
+});
